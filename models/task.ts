b683edf3891be9cce2de904959fa3aa3ddcbd74c
houssem <houssem.macbook@gmail.com>
2023-05-05 23:34:17 +0100

    add timestamps

--- models/task.ts
+++ models/task.ts
@@ -1,24 +1,30 @@
 import { DataTypes } from "sequelize";
 import sequelize from "@/helpers/database";
 
-const Task = sequelize.define("task", {
-  id: {
-    type: DataTypes.BIGINT,
-    autoIncrement: true,
-    primaryKey: true,
+const Task = sequelize.define(
+  "task",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING(40),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  title: {
-    type: DataTypes.STRING(40),
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING(255),
-    allowNull: false,
-  },
-  status: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default Task;
